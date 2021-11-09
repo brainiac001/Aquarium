08ed9c8ba5fc50126bad84d8af31574d0850deb9
Oluwadamilare Idowu Joshua <oluwadamilareidowujoshua@gmail.com>
2021-11-09 19:26:15 +0100

    Update HelpActivity.kt

--- app/src/main/java/com/idootech/aquarium/HelpActivity.kt
+++ app/src/main/java/com/idootech/aquarium/HelpActivity.kt
@@ -25,7 +25,7 @@ class HelpActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-6887344842994800/8697117280",
+            "ca-app-pub-3940256099942544/1033173712",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -100,7 +100,7 @@ class HelpActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-6887344842994800/8697117280",
+            "ca-app-pub-3940256099942544/1033173712",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
