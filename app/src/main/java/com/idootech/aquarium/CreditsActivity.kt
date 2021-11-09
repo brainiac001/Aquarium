238545dab00ebb75ae9d887c0113c57e6c6fef1e
Oluwadamilare Idowu Joshua <oluwadamilareidowujoshua@gmail.com>
2021-11-09 19:23:58 +0100

    Update CreditsActivity.kt

--- app/src/main/java/com/idootech/aquarium/CreditsActivity.kt
+++ app/src/main/java/com/idootech/aquarium/CreditsActivity.kt
@@ -24,7 +24,7 @@ class CreditsActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-6887344842994800/8697117280",
+            "ca-app-pub-3940256099942544/1033173712",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -98,7 +98,7 @@ class CreditsActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-6887344842994800/8697117280",
+            "ca-app-pub-3940256099942544/1033173712",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
